3ed10ef8f84dc50a70506f87a2200396a6ab1c8d
GustavLevin <gustav_levin@hotmail.com>
2024-10-15 00:48:17 +0200

    Final updates

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // Ersätt med namnet på ditt GitHub-repo
+  base: "/portfolio/", 
 })
